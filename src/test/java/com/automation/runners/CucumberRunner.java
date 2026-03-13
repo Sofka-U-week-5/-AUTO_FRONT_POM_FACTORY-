d8a2c8c15e2214950be43171f13906bbd83b2753
Omar Ortiz  (Tuto Omar) <103438028+tutoOmar@users.noreply.github.com>
2026-03-13 00:59:59 +0000

    Se hacen cambios puntuales para lograr correr con gradle

--- src/test/java/com/automation/runners/CucumberRunner.java
+++ src/test/java/com/automation/runners/CucumberRunner.java
@@ -11,7 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.automation.steps")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.automation.steps,com.automation.stepsdefinitions")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @ignore")
 public class CucumberRunner {
 }
